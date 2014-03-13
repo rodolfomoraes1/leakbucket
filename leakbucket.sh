b97cc8b3ce24fab00651bab5b20f79e8c6516acc
Jeanderson Candido <jeandersonbc@gmail.com>
2014-03-13 00:15:43 -0300

    Initial paramater validation

--- leakbucket.sh
+++ leakbucket.sh
@@ -2,3 +2,23 @@
 #
 # Contributors:
 #     Jeanderson Candido - http://jeandersonbc.github.io
+#
+ARGCOUNTER=1
+
+displayHelp() {
+    printf "\nUsage: ./leakbucket.sh [start [dirname]| stop | status]\n\n"
+	printf "  ./leakbucket.sh start [dirname]\n"
+	printf "    Starts the file monitoring on the given \"dirname\".\n"
+	printf "    If none provided, current directory is used.\n\n"
+
+    printf "  ./leakbucket.sh stop\n"
+    printf "    Stops the file monitoring\n\n"
+
+    printf "  ./leakbucket.sh status\n"
+    printf "    Returns \"started\" if running, \"stopped\" otherwise.\n\n"
+}
+
+# Input validation
+if [[ $ARGCOUNTER -ne $# ]]; then
+   displayHelp
+fi
