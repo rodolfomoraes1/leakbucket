c7ddd2f9c8d7a5d4fa8d3439aea581103ff31523
Jeanderson Candido <jeandersonbc@gmail.com>
2014-03-13 01:34:51 -0300

    Fixed bad indentation

--- leakbucket.sh
+++ leakbucket.sh
@@ -10,9 +10,9 @@ STATUS=$STOPPED
 
 displayHelp() {
     printf "Usage: ./leakbucket.sh [start [dirname]| stop | status]\n\n"
-	printf "  ./leakbucket.sh start [dirname]\n"
-	printf "    Starts the file monitoring on the given \"dirname\".\n"
-	printf "    If none provided, current directory is used.\n\n"
+    printf "  ./leakbucket.sh start [dirname]\n"
+    printf "    Starts the file monitoring on the given \"dirname\".\n"
+    printf "    If none provided, current directory is used.\n\n"
 
     printf "  ./leakbucket.sh stop\n"
     printf "    Stops the file monitoring\n\n"
@@ -22,7 +22,7 @@ displayHelp() {
 }
 
 executeStartCommand() {
-	[ ! -d $1 ] && echo "Fatal error: Directory not found!" && exit 1
+    [ ! -d $1 ] && echo "Fatal error: Directory not found!" && exit 1
     echo "Command not implemented yet"
 }
 
@@ -34,23 +34,23 @@ case $1 in
    start)
        [ $# -gt $MAX_ARGC ] && displayHelp && exit 1
 
-	   # Assume current directory
-	   dir="."
-	   if [[ $# -eq $MAX_ARGC ]]; then
-	       dir=$2
+       # Assume current directory
+       dir="."
+       if [[ $# -eq $MAX_ARGC ]]; then
+           dir=$2
        fi
-	   executeStartCommand $dir
-	   ;;
+       executeStartCommand $dir
+       ;;
    stop)
        [ $# -gt 1 ] && displayHelp && exit 1
-	   executeStopCommand
-	   ;;
+       executeStopCommand
+       ;;
    status)
        [ $# -gt 1 ] && displayHelp && exit 1
-	   echo $STATUS
-	   ;;
+       echo $STATUS
+       ;;
    *)
-	   # Unknown command
-	   displayHelp
-	   ;;
+       # Unknown command
+       displayHelp
+       ;;
 esac
