cf0370a24cdf2fa3c514962d0610c1b6c487bc3e
Jeanderson Candido <jeandersonbc@gmail.com>
2014-03-13 01:21:38 -0300

    Implemented parameter validation

--- leakbucket.sh
+++ leakbucket.sh
@@ -3,10 +3,13 @@
 # Contributors:
 #     Jeanderson Candido - http://jeandersonbc.github.io
 #
-ARGCOUNTER=1
+MAX_ARGC=2
+RUNNING="started"
+STOPPED="stopped"
+STATUS=$STOPPED
 
 displayHelp() {
-    printf "\nUsage: ./leakbucket.sh [start [dirname]| stop | status]\n\n"
+    printf "Usage: ./leakbucket.sh [start [dirname]| stop | status]\n\n"
 	printf "  ./leakbucket.sh start [dirname]\n"
 	printf "    Starts the file monitoring on the given \"dirname\".\n"
 	printf "    If none provided, current directory is used.\n\n"
@@ -18,7 +21,36 @@ displayHelp() {
     printf "    Returns \"started\" if running, \"stopped\" otherwise.\n\n"
 }
 
-# Input validation
-if [[ $ARGCOUNTER -ne $# ]]; then
-   displayHelp
-fi
+executeStartCommand() {
+	[ ! -d $1 ] && echo "Fatal error: Directory not found!" && exit 1
+    echo "Command not implemented yet"
+}
+
+executeStopCommand() {
+    echo "Command not implemented yet"
+}
+
+case $1 in
+   start)
+       [ $# -gt $MAX_ARGC ] && displayHelp && exit 1
+
+	   # Assume current directory
+	   dir="."
+	   if [[ $# -eq $MAX_ARGC ]]; then
+	       dir=$2
+       fi
+	   executeStartCommand $dir
+	   ;;
+   stop)
+       [ $# -gt 1 ] && displayHelp && exit 1
+	   executeStopCommand
+	   ;;
+   status)
+       [ $# -gt 1 ] && displayHelp && exit 1
+	   echo $STATUS
+	   ;;
+   *)
+	   # Unknown command
+	   displayHelp
+	   ;;
+esac
